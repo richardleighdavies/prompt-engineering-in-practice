3c8928c0a0c35437723d2317a605f9bed10245ae
Rafael Fischer <rafaelsanfischer@gmail.com>
2025-07-28 18:12:43 -0300

    remove: nome da filha

--- nomeDaFilha.js
+++
@@ -1,9 +0,0 @@
-const nomeDaFilha = (nomeCompletoDaMae, nomeCompletoDoPai, nomeDaFilha) => {
-  // 'Rafael Santos Fischer' -> ['Rafael', 'Santos', 'Fischer'] -> 'Fischer'
-  const tamanhoDoNomeCompletoDoPai = nomeCompletoDoPai.split(" ").length;
-  const sobreNomeDoPai =
-    nomeCompletoDoPai.split(" ")[tamanhoDoNomeCompletoDoPai - 1];
-  const sobreNomeDaMae = nomeCompletoDaMae.split(" ")[1];
-  const nomeCompletoDaFilha = `${nomeDaFilha} ${sobreNomeDaMae} ${sobreNomeDoPai}`;
-  return nomeCompletoDaFilha;
-};
