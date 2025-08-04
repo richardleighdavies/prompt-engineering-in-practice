c8329a0a86e05511ec9dabe716c45bdc55b9c7fe
Rafael Fischer <rafaelsanfischer@gmail.com>
2025-08-04 19:51:19 -0300

    faet: generating goldens

--- chapter_15/section_02/rag/goldens.py
+++ chapter_15/section_02/rag/goldens.py
@@ -2,26 +2,34 @@
 import pickle
 import json
 from deepeval.synthesizer import Synthesizer
+from deepeval.synthesizer.config import ContextConstructionConfig
 from dotenv import load_dotenv
 load_dotenv()
 
-synthesizer = Synthesizer()
 with open("data/menu.json", "r", encoding="utf-8") as json_file:
     data = json.load(json_file)
 
 with open("data/menu.txt", "w", encoding="utf-8") as txt_file:
     txt_file.write(json.dumps(data, indent=2))
+ 
+synthesizer = Synthesizer()
 
-goldens = []
-for i in range(3):  
-    try:
-        golden = synthesizer.generate_goldens_from_docs(
-            document_paths=["data/menu.txt"]
-        )
-        goldens.append(golden)
-        time.sleep(2)  
-    except Exception as e:
-        print(f"Error {i}: {e}")
+context_config = ContextConstructionConfig(
+    max_contexts_per_document=1,    
+    chunk_size=512,                 
+    chunk_overlap=0                
+)
+
+try:
+    goldens = synthesizer.generate_goldens_from_docs(
+        document_paths=["data/menu.txt"],
+        max_goldens_per_context=1,                    
+        context_construction_config=context_config,
+        include_expected_output=True                  
+    )   
+except Exception as e:
+    print(f"Erro ao gerar goldens: {e}")
+    goldens = []
 
 with open("goldens.pkl", "wb") as f:
     pickle.dump(goldens, f)
